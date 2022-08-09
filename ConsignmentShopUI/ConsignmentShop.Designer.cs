041017803881d2699535315852729f6a1dced211
burak karacakaya <burakaracakaya@gmail.com>
2022-08-09 12:21:04 +0300

    Add project files.

---
+++ ConsignmentShopUI/ConsignmentShop.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ConsignmentShopUI
+{
+    partial class ConsignmentShop
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerText = new System.Windows.Forms.Label();
+            this.itemsListbox = new System.Windows.Forms.ListBox();
+            this.itemsListboxLabel = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.shoppingCartListboxLabel = new System.Windows.Forms.Label();
+            this.shoppingCartListbox = new System.Windows.Forms.ListBox();
+            this.makePurchae = new System.Windows.Forms.Button();
+            this.vebdorListboxLabel = new System.Windows.Forms.Label();
+            this.vendorListbox = new System.Windows.Forms.ListBox();
+            this.storeProfitLabel = new System.Windows.Forms.Label();
+            this.storeProfitValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // headerText
+            // 
+            this.headerText.AutoSize = true;
+            this.headerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.headerText.Location = new System.Drawing.Point(12, 9);
+            this.headerText.Name = "headerText";
+            this.headerText.Size = new System.Drawing.Size(396, 37);
+            this.headerText.TabIndex = 0;
+            this.headerText.Text = "Consignment Shot Demo";
+            // 
+            // itemsListbox
+            // 
+            this.itemsListbox.FormattingEnabled = true;
+            this.itemsListbox.ItemHeight = 20;
+            this.itemsListbox.Location = new System.Drawing.Point(12, 103);
+            this.itemsListbox.Name = "itemsListbox";
+            this.itemsListbox.Size = new System.Drawing.Size(336, 284);
+            this.itemsListbox.TabIndex = 1;
+            // 
+            // itemsListboxLabel
+            // 
+            this.itemsListboxLabel.AutoSize = true;
+            this.itemsListboxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.itemsListboxLabel.Location = new System.Drawing.Point(12, 80);
+            this.itemsListboxLabel.Name = "itemsListboxLabel";
+            this.itemsListboxLabel.Size = new System.Drawing.Size(103, 20);
+            this.itemsListboxLabel.TabIndex = 2;
+            this.itemsListboxLabel.Text = "Store Items";
+            // 
+            // addToCart
+            // 
+            this.addToCart.Location = new System.Drawing.Point(374, 213);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(159, 54);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "Add To Cart ->";
+            this.addToCart.UseVisualStyleBackColor = true;
+            this.addToCart.Click += new System.EventHandler(this.addToCart_Click);
+            // 
+            // shoppingCartListboxLabel
+            // 
+            this.shoppingCartListboxLabel.AutoSize = true;
+            this.shoppingCartListboxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.shoppingCartListboxLabel.Location = new System.Drawing.Point(555, 80);
+            this.shoppingCartListboxLabel.Name = "shoppingCartListboxLabel";
+            this.shoppingCartListboxLabel.Size = new System.Drawing.Size(124, 20);
+            this.shoppingCartListboxLabel.TabIndex = 5;
+            this.shoppingCartListboxLabel.Text = "Shopping Cart";
+            // 
+            // shoppingCartListbox
+            // 
+            this.shoppingCartListbox.FormattingEnabled = true;
+            this.shoppingCartListbox.ItemHeight = 20;
+            this.shoppingCartListbox.Location = new System.Drawing.Point(555, 103);
+            this.shoppingCartListbox.Name = "shoppingCartListbox";
+            this.shoppingCartListbox.Size = new System.Drawing.Size(336, 284);
+            this.shoppingCartListbox.TabIndex = 4;
+            // 
+            // makePurchae
+            // 
+            this.makePurchae.Location = new System.Drawing.Point(732, 393);
+            this.makePurchae.Name = "makePurchae";
+            this.makePurchae.Size = new System.Drawing.Size(159, 54);
+            this.makePurchae.TabIndex = 6;
+            this.makePurchae.Text = "Purchase";
+            this.makePurchae.UseVisualStyleBackColor = true;
+            this.makePurchae.Click += new System.EventHandler(this.makePurchae_Click);
+            // 
+            // vebdorListboxLabel
+            // 
+            this.vebdorListboxLabel.AutoSize = true;
+            this.vebdorListboxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vebdorListboxLabel.Location = new System.Drawing.Point(12, 498);
+            this.vebdorListboxLabel.Name = "vebdorListboxLabel";
+            this.vebdorListboxLabel.Size = new System.Drawing.Size(76, 20);
+            this.vebdorListboxLabel.TabIndex = 8;
+            this.vebdorListboxLabel.Text = "Vendors";
+            // 
+            // vendorListbox
+            // 
+            this.vendorListbox.FormattingEnabled = true;
+            this.vendorListbox.ItemHeight = 20;
+            this.vendorListbox.Location = new System.Drawing.Point(12, 521);
+            this.vendorListbox.Name = "vendorListbox";
+            this.vendorListbox.Size = new System.Drawing.Size(336, 284);
+            this.vendorListbox.TabIndex = 7;
+            // 
+            // storeProfitLabel
+            // 
+            this.storeProfitLabel.AutoSize = true;
+            this.storeProfitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.storeProfitLabel.Location = new System.Drawing.Point(555, 498);
+            this.storeProfitLabel.Name = "storeProfitLabel";
+            this.storeProfitLabel.Size = new System.Drawing.Size(101, 20);
+            this.storeProfitLabel.TabIndex = 9;
+            this.storeProfitLabel.Text = "Store Profit";
+            // 
+            // storeProfitValue
+            // 
+            this.storeProfitValue.AutoSize = true;
+            this.storeProfitValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.storeProfitValue.Location = new System.Drawing.Point(688, 498);
+            this.storeProfitValue.Name = "storeProfitValue";
+            this.storeProfitValue.Size = new System.Drawing.Size(54, 20);
+            this.storeProfitValue.TabIndex = 10;
+            this.storeProfitValue.Text = "$0.00";
+            // 
+            // ConsignmentShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 832);
+            this.Controls.Add(this.storeProfitValue);
+            this.Controls.Add(this.storeProfitLabel);
+            this.Controls.Add(this.vebdorListboxLabel);
+            this.Controls.Add(this.vendorListbox);
+            this.Controls.Add(this.makePurchae);
+            this.Controls.Add(this.shoppingCartListboxLabel);
+            this.Controls.Add(this.shoppingCartListbox);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.itemsListboxLabel);
+            this.Controls.Add(this.itemsListbox);
+            this.Controls.Add(this.headerText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ConsignmentShop";
+            this.Text = "Consignment Shop Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.ListBox itemsListbox;
+        private System.Windows.Forms.Label itemsListboxLabel;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.Label shoppingCartListboxLabel;
+        private System.Windows.Forms.ListBox shoppingCartListbox;
+        private System.Windows.Forms.Button makePurchae;
+        private System.Windows.Forms.Label vebdorListboxLabel;
+        private System.Windows.Forms.ListBox vendorListbox;
+        private System.Windows.Forms.Label storeProfitLabel;
+        private System.Windows.Forms.Label storeProfitValue;
+    }
+}
---
+++ ConsignmentShopUI/ConsignmentShop.cs
@@ -0,0 +1,141 @@
+﻿using ConsignmentShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsignmentShopUI
+{
+    public partial class ConsignmentShop : Form
+    {
+        private Store store = new Store();
+        private List<Item> shoppingCartData = new List<Item>();
+        
+        BindingSource itemsBinding = new BindingSource();
+        BindingSource cartBinding = new BindingSource();
+        BindingSource vendorsBinding = new BindingSource();
+
+        private decimal storeProfit = 0;
+
+
+
+        public ConsignmentShop()
+        {
+            InitializeComponent();
+            SetupData();
+
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold == false).ToList();
+            itemsListbox.DataSource = itemsBinding;
+
+            itemsListbox.DisplayMember = "Display";
+            itemsListbox.ValueMember = "Display";
+
+
+            cartBinding.DataSource = shoppingCartData;
+            shoppingCartListbox.DataSource = cartBinding;
+
+            shoppingCartListbox.DisplayMember = "Display";
+            shoppingCartListbox.ValueMember = "Display";
+
+            vendorsBinding.DataSource = store.Vendors;
+            vendorListbox.DataSource = vendorsBinding;
+
+            vendorListbox.DisplayMember = "Display";
+            vendorListbox.ValueMember = "Display";
+
+            
+        }
+
+        private void SetupData()
+        {
+
+            store.Vendors.Add(new Vendor { FirstName = "Burak", LastName = "Oceans" });
+            store.Vendors.Add(new Vendor { FirstName = "Beykay", LastName = "Jones" });
+            store.Vendors.Add(new Vendor { FirstName = "Jack", LastName = "Jones" });
+
+            store.Items.Add(new Item
+            {
+                Title = "Crime and Punishment",
+                Description = "Crime and Punishment follows " +
+                "the mental anguish and moral dilemmas of Rodion Raskolnikov, " +
+                "an impoverished ex-student in Saint Petersburg who plans to kill an " +
+                "unscrupulous pawnbroker, an old woman " +
+                "who stores money and valuable objects in her flat.",
+                Price = 24.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Les Miserables",
+                Description = "Les Misérables is a show about courage, " +
+                "love, heartbreak, passion, " +
+                "and the resilience of the human spirit—themes which undoubtedly transcend time and place. " +
+                "Perhaps the most relevant themes, however, are related to the dignity of the human person.",
+                Price = 19.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "A Tale of Two Cities",
+                Description = "A book about a revolution",
+                Price = 9.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Harry Potter Book 1",
+                Description = "A book about a boy and magic",
+                Price = 14.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Name = "Seconds are Better";
+        }
+
+        private void addToCart_Click(object sender, EventArgs e)
+        {
+            // Figure out what is selected from the items list
+            // Copy that tem to the shopping cart
+            // Do we remove the item from the items list? - No
+
+            Item selectedItem = (Item)itemsListbox.SelectedItem;
+
+            shoppingCartData.Add(selectedItem);
+
+            cartBinding.ResetBindings(false);
+
+        }
+
+        private void makePurchae_Click(object sender, EventArgs e)
+        {
+            // Mark each item in the cars as sold
+            // Clear the cart
+
+            foreach (Item item in shoppingCartData)
+            {
+                item.Sold = true;
+                item.Owner.PaymentDue += (decimal)item.Owner.Commission * item.Price;
+                storeProfit += ( 1 - (decimal)item.Owner.Commission) * item.Price;
+
+            }
+
+            shoppingCartData.Clear();
+
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold == false).ToList();
+
+            storeProfitValue.Text = string.Format("${0}", storeProfit);
+
+            cartBinding.ResetBindings(false);
+            itemsBinding.ResetBindings(false);
+            vendorsBinding.ResetBindings(false);
+        }
+    }
+}
